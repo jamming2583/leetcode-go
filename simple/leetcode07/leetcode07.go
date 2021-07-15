82d48a008c04c0cffe0c5b9dd968be3249ce1513
DESKTOP-PBMP6M4\ltm <jamming2583@hotmail.com>
2021-07-15 22:50:38 +0800

    simple-07

--- simple/leetcode07/leetcode07.go
+++ simple/leetcode07/leetcode07.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	println(reverse(0))
+	println(reverse1(0))
 }
 
 //考的是取余，整除啥的
@@ -25,15 +25,14 @@ func reverse(x int) int {
 /**
 人家写的
 */
-func reverse1(x int) int {
-	var res int
+func reverse1(x int) (rev int) {
 	for x != 0 {
 		// 利用go语言的特性，(temp*10)/10 != temp 如果溢出则两边不等
-		if temp := int32(res); (temp*10)/10 != temp {
+		if temp := int32(rev); (temp*10)/10 != temp {
 			return 0
 		}
-		res = res*10 + x%10
-		x = x / 10
+		rev = rev*10 + x%10
+		x /= 10
 	}
-	return res
+	return
 }
